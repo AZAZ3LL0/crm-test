8d435f8fba3cf84a85d41d3863a3ea46c05fcf5c
Самат Садриев <samatsadriev@MacBook-Air-Samat.local>
2025-11-27 07:16:46 +0300

    first commit

--- database.py
+++ database.py
@@ -3,13 +3,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./crm.db"
 
-# Создаём движок для SQLite
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-
-# Сессия
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# База для моделей
 Base = declarative_base()
